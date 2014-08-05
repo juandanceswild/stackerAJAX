ee8725831c735800ceccb5ade4a89933029bd246
juandanceswild <juancoob@gmail.com>
2014-08-05 17:33:23 -0400

    Added inspiration finder

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='answerers']").val();
+		getAnswerers(tags);
+	});
+
+
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -37,6 +47,31 @@ var showQuestion = function(question) {
 							'</p>' +
  							'<p>Reputation: ' + question.owner.reputation + '</p>'
 	);
+
+	return result;
+};
+
+
+// this function takes the ANSWERER object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showAnswerer = function(answerer) {
+	
+	// clone our result template code
+	var result = $('.templates .answerers').clone();
+	
+	// Set the answerer properties in result
+	var answererElem = result.find('.answerer-name a');
+	answererElem.attr('href', answerer.user.link);
+	answererElem.text(answerer.user.display_name);
+
+	// set the user reputation property in result
+	var reputation = result.find('.reputation');
+	reputation.text(answerer.user.reputation);
+
+	// set the #views for question property in result
+	var score = result.find('.score');
+	score.text(answerer.score);
+
 
 	return result;
 };
@@ -90,3 +125,32 @@ var getUnanswered = function(tags) {
 
 
 
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getAnswerers = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {				site: 'stackoverflow'
+								};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+tags+"/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(tags, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
